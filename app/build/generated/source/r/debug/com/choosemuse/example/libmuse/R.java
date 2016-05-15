8e45af0c9f2bccf38725e3840868dcedfa9e1b00
Derek Jacoby <derekja@gmail.com>
2016-05-14 17:57:01 -0700

    base working, need UI changes

--- app/build/generated/source/r/debug/com/choosemuse/example/libmuse/R.java
+++ app/build/generated/source/r/debug/com/choosemuse/example/libmuse/R.java
@@ -18,25 +18,26 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int acc_x=0x7f080009;
-        public static final int acc_y=0x7f08000a;
-        public static final int acc_z=0x7f08000b;
-        public static final int action_settings=0x7f080012;
-        public static final int con_status=0x7f080004;
+        public static final int acc_x=0x7f08000a;
+        public static final int acc_y=0x7f08000b;
+        public static final int acc_z=0x7f08000c;
+        public static final int action_settings=0x7f080013;
+        public static final int button=0x7f080004;
+        public static final int con_status=0x7f080005;
         public static final int connect=0x7f080002;
         public static final int disconnect=0x7f080003;
-        public static final int eeg_af7=0x7f080006;
-        public static final int eeg_af8=0x7f080007;
-        public static final int eeg_tp10=0x7f080008;
-        public static final int eeg_tp9=0x7f080005;
-        public static final int elem1=0x7f08000c;
-        public static final int elem2=0x7f08000d;
-        public static final int elem3=0x7f08000e;
-        public static final int elem4=0x7f08000f;
+        public static final int eeg_af7=0x7f080007;
+        public static final int eeg_af8=0x7f080008;
+        public static final int eeg_tp10=0x7f080009;
+        public static final int eeg_tp9=0x7f080006;
+        public static final int elem1=0x7f08000d;
+        public static final int elem2=0x7f08000e;
+        public static final int elem3=0x7f08000f;
+        public static final int elem4=0x7f080010;
         public static final int muses_spinner=0x7f080000;
-        public static final int pause=0x7f080011;
+        public static final int pause=0x7f080012;
         public static final int refresh=0x7f080001;
-        public static final int version=0x7f080010;
+        public static final int version=0x7f080011;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
